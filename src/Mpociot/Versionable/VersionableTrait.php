b4aae986e4aae96af53f807d094a6bfd67ddd909
Charles Sterling <charles@fitnimbus.com>
2021-04-22 15:31:53 -0700

    Added class morph

--- src/Mpociot/Versionable/VersionableTrait.php
+++ src/Mpociot/Versionable/VersionableTrait.php
@@ -173,7 +173,7 @@ trait VersionableTrait
             $class                     = $this->getVersionClass();
             $version                   = new $class();
             $version->versionable_id   = $this->getKey();
-            $version->versionable_type = get_class($this);
+            $version->versionable_type = $this->getMorphClass();
             $version->user_id          = $this->getAuthUserId();
             $version->model_data       = serialize($this->attributesToArray());
 
